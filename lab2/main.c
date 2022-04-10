3f4d37f92feed3adb31fba99b5cff14a0e41825e
IMrXAT <96072493+IMrXAT@users.noreply.github.com>
2022-04-10 19:28:36 +0700

    Add files via upload

---
+++ lab2/main.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define TAU (0.000002)
+#define MATRIX_SIZE 1000
+#define EPS 0.000001
+
+void testVector(const double* vector){
+    printf("vector:\n");
+    for (int i = 0; i < MATRIX_SIZE; i++){
+        printf("%.2f\n", vector[i]);
+    }
+}
+
+void initMatrix(double* matrix, double *vectorX, double *vectorB){
+    for (int i = 0; i < MATRIX_SIZE; i++){
+        for (int j = 0; j < MATRIX_SIZE; j++){
+            if (i == j){
+                matrix[i*MATRIX_SIZE+j] = 2.0;
+            }
+            else matrix[i*MATRIX_SIZE+j] = 1.0;
+        }
+        vectorX[i] = 0;
+        vectorB[i] = MATRIX_SIZE+1;
+    }
+}
+
+void mulMatrixOnVector(const double* matrix, const double* vector, double* answer){
+    double* tmp = (double*) malloc(sizeof(double)*MATRIX_SIZE);
+
+    for (int i = 0; i < MATRIX_SIZE; i++){
+            tmp[i] = 0;
+            for (int k = 0; k < MATRIX_SIZE; k++){
+                tmp[i] += matrix[i*MATRIX_SIZE+k]*vector[k];
+            }
+    }
+    for (int i = 0; i < MATRIX_SIZE; i++){
+        answer[i] = tmp[i];
+    }
+    free(tmp);
+}
+
+void subtractionVectors(const double* vector1, const double* vector2, double* answer){
+    for (int i = 0; i < MATRIX_SIZE; i++){
+        answer[i] = vector1[i] - vector2[i];
+    }
+}
+
+void mulVectorOnConst(const double* vector, double* answer){
+    for (int i = 0; i < MATRIX_SIZE; i++){
+        answer[i] = vector[i]*TAU;
+    }
+}
+
+double vectorNorm(const double* vector){
+    double tmp = 0;
+    for (int i = 0; i < MATRIX_SIZE; i++){
+        tmp += pow(vector[i], 2);
+    }
+    return sqrt(tmp);
+}
+
+
+int isFinish(const double* vector1, const double normVectorB){
+    if ((vectorNorm(vector1) / normVectorB) < EPS){
+        return 1;
+    }
+    else return 0;
+}
+
+// (Ax-b)
+void makeVectorDiff(double* matrix, double* vectorX, double* vectorB, double* answer){
+    mulMatrixOnVector(matrix, vectorX, answer);
+    subtractionVectors(answer, vectorB, answer);
+}
+
+
+void algorithm(double* matrix, double* vectorX, double* vectorB){
+    double *vectorDiff = (double*) malloc (sizeof(double)*MATRIX_SIZE);
+    makeVectorDiff(matrix, vectorX, vectorB, vectorDiff);
+    double normVectorB = vectorNorm(vectorB);
+    while(isFinish(vectorDiff, normVectorB) == 0){
+        mulVectorOnConst(vectorDiff, vectorDiff);
+        subtractionVectors(vectorX, vectorDiff, vectorX);
+        makeVectorDiff(matrix, vectorX, vectorB, vectorDiff);
+    }
+    free(vectorDiff);
+}
+
+
+int main(){
+    double *matrix = (double*) malloc(sizeof(double)*MATRIX_SIZE*MATRIX_SIZE);
+    double *vectorX = (double*) malloc(sizeof(double)*MATRIX_SIZE);
+    double *vectorB = (double*) malloc(sizeof(double )*MATRIX_SIZE);
+
+    initMatrix(matrix, vectorX, vectorB);
+    algorithm(matrix,vectorX, vectorB);
+    testVector(vectorX);
+
+    free(matrix);
+    free(vectorX);
+    free(vectorB);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
